19ad3d85a64acfb6accb188d1cc1d50813b8fd31
Abrarul Rhythm <[....email>
2025-12-27 00:21:28 +0600

    added next js project food bit

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FoodsPage = () => {
+    return (
+        <section className='foods py-10 md:py-12 lg:py-20'>
+            <div className='container'>
+                Foods
+            </div>
+        </section>
+    );
+};
+
+export default FoodsPage;
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaBars } from "react-icons/fa6";
+
+const Header = () => {
+    return (
+        <header className="px-5 py-6 flex justify-between gap-5 bg-stone-800 items-center">
+            <div className='container'>
+                <div className='flex justify-between items-center'>
+                    <Link href='/'>
+                        <img src='/logo.png' className="w-[160px]" />
+                    </Link>
+                    <div>
+                        <div className='text-[22px] block md:hidden'>
+                            <FaBars />
+                        </div>
+                        <div className='space-x-5 hidden md:block'>
+                            <Link className="btn" href='/foods'>Foods</Link>
+                            <Link className="btn" href='/reviews'>Reviews</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
