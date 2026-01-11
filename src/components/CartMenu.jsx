5be8f563b98c786cd4f36f456c5cf945ef1893f0
Abrarul Rhythm <[....email>
2026-01-12 00:35:52 +0600

    fixed some image releted issue

--- src/components/CartMenu.jsx
+++ src/components/CartMenu.jsx
@@ -4,6 +4,7 @@ import React, { use } from 'react';
 import CartClose from './button/CartClose';
 import { CartContext } from '@/context/CartProvider';
 import { IoClose } from 'react-icons/io5';
+import Image from 'next/image';
 
 const CartMenu = ({ oepnCart, setOpenCart }) => {
     const { cart, setCart } = use(CartContext);
@@ -14,7 +15,7 @@ const CartMenu = ({ oepnCart, setOpenCart }) => {
     }
 
     return (
-        <div className={`${oepnCart ? 'right-0' : '-right-88'} duration-500 w-87.5 bg-white h-screen fixed top-0 z-30 shadow-2xl overflow-y-scroll text-black`}>
+        <div className={`${oepnCart ? 'right-0' : '-right-88'} duration-500 w-87.5 bg-white h-screen fixed top-0 z-40 shadow-2xl overflow-y-scroll text-black`}>
             <CartClose
                 setOpenCart={setOpenCart}
             ></CartClose>
@@ -22,7 +23,12 @@ const CartMenu = ({ oepnCart, setOpenCart }) => {
             <div className='px-4 pt-5 pb-4'>
                 {(!cart || cart.length === 0) ? (
                     <div className='empty-cart flex flex-col items-center justify-center'>
-                        <img src="/empty-cart.png" className='mb-4' alt="" />
+                        <Image
+                            src='/empty-cart.png'
+                            width={128}
+                            height={128}
+                            alt='Cart Icon'
+                        ></Image>
                         <h3 className='text-2xl font-semibold'>Your Cart is Empty</h3>
                     </div>
                 ) : (
@@ -33,7 +39,13 @@ const CartMenu = ({ oepnCart, setOpenCart }) => {
                                 return (
                                     <div key={item.id} className='flex items-center justify-between mb-4'>
                                         <div className='flex items-center'>
-                                            <img src={item.foodImg} className='w-20 h-20 object-cover rounded-md' alt="" />
+                                            <Image
+                                                width={80}
+                                                height={80}
+                                                src={item.foodImg}
+                                                alt='Food Image'
+                                                className='object-cover rounded-md'
+                                            ></Image>
                                             <div className='ml-4'>
                                                 <h4 className='text-base font-semibold mb-px'>{item.title}</h4>
                                                 <h3 className='text-lg font-semibold text-orange-500 mb-2'>${item.price}</h3>
